3ec6b8db6e29af8fa7d5514cfa9a206279f8d6ce
torukino <toruk0131@gmail.com>
2023-12-29 02:09:10 +0900

    client2

--- src/app/client2/page.tsx
+++ src/app/client2/page.tsx
@@ -1,19 +1,49 @@
-'use server'
+'use client'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import { useFormState } from 'react-dom'
+import { useDebouncedCallback } from 'use-debounce'
+import { formDataAction } from '@/actions/actions'
+import { Button } from '@/catalyst/button'
+import { Field, Label } from '@/catalyst/fieldset'
+import { Input } from '@/catalyst/input'
 
-import { submitAction } from '@/actions/actions'
+export default function Page() {
+	const searchParams = useSearchParams()
+	const pathname = usePathname()
+	const { replace } = useRouter()
+	const [name, setName] = useState('')
 
-export default async function Page() {
+	const [formState, formAction] = useFormState(formDataAction, { message: null })
+
+	const setNameFunc = useDebouncedCallback((term: string) => {
+		const params = new URLSearchParams(searchParams)
+		if (term) params.set('name', term)
+		else params.delete('name')
+
+		console.log(`${pathname}?${params.toString()}`)
+		console.log(term)
+		setName(term)
+
+		replace(`${pathname}?${params.toString()}`)
+	}, 300)
+
 	return (
-		<div className="h-screen flex flex-col">
-			<div className="m-4 text-3xl">Page functionは server</div>
-			<div className="m-4 text-xl">注意:Page functionは非同期関数</div>
-
-			<form action={submitAction} className="m-4">
-				<input className="border border-gray-400 rounded px-4 py-2" name="name" placeholder="name" />
-				<button className="primaryButton bg-blue-700 px-4 py-2 rounded text-gray-100 hover:bg-blue-400" type="submit">
-					Submit
-				</button>
+		<div className="h-screen">
+			<div className="m-4 text-3xl">Pageがclient</div>
+			<p>usePathname, useRouter, useSearchParams,</p>
+			<p>useDebouncedCallback, useStateとかを使った</p>
+			<form action={formAction} className="m-4">
+				<Field>
+					<Label>名前：</Label>
+					<Input name="name" onChange={e => setNameFunc(e.target.value)} placeholder="名前を入力" />
+				</Field>
+				<Button color="lime" type="submit">
+					Save changes
+				</Button>
 			</form>
+			{name && <div className="bg-yellow-100 m-4 text-3xl">{name}</div>}
+			<div>{formState.message}</div>
 		</div>
 	)
 }
